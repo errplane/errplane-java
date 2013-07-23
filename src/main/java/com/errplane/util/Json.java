a5dd4645ab540a776ffe87825b3929bffee63b34
John Shahid <jvshahid@gmail.com>
2013-07-23 12:50:31 -0400

    remove an unused method.

--- src/main/java/com/errplane/util/Json.java
+++ src/main/java/com/errplane/util/Json.java
@@ -1,7 +1,5 @@
 package com.errplane.util;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -65,65 +63,5 @@ public class Json {
     add(buffer, obj);
     return buffer.toString();
   }
-
-	@SuppressWarnings("unchecked")
-  public static String marshalToJsonOld(Map<String,Object> hash) {
-	    String jsonStr = "{";
-	    int ind = 0;
-	    int count = hash.size();
-	    for (Map.Entry<String,Object> entry: hash.entrySet()) {
-	    	Object obj = entry.getValue();
-	    	if (obj instanceof List) {// backtrace or breadcrumbs
-	            jsonStr += "\"" + entry.getKey() + "\":[";
-	            int arrayInd = 0;
-	            int arrayCount = ((ArrayList<String>)obj).size();
-	            for (String arrVal: (ArrayList<String>)obj) {
-	                jsonStr += "\"" + arrVal + "\"";
-	                if (arrayInd < (arrayCount-1)) {
-	                    jsonStr += ",";
-	                }
-	                arrayInd++;
-	            }
-                jsonStr += "]";
-	        }
-	        else if (obj instanceof Map) {// handles HashMap<String,String>
-	            jsonStr += "\"" + entry.getKey() + "\":{";
-	            HashMap<String,String> objHash = (HashMap<String,String>)obj;
-	            int subInd = 0;
-	            int subCount = objHash.size();
-	            for (Map.Entry<String, String> subEntry: objHash.entrySet()) {
-                    jsonStr += "\"" + subEntry.getKey() + "\":\"" +
-                    		subEntry.getValue() + "\"";
-
-	                if (subInd < (subCount-1)) {
-	                    jsonStr += ",";
-	                }
-	                subInd++;
-	            }
-                jsonStr += "}";
-	        }
-	        else {
-
-	        	// custom data could start with a bracket - if so then don't include the quotes
-	        	if (obj.toString().charAt(0) == '{') {
-	        		jsonStr += "\"" + entry.getKey() + "\":" +
-	        				obj;
-	        	}
-
-	        	if (obj instanceof Number) {
-	        	  jsonStr += "\"" + entry.getKey() + "\":" + obj;
-	        	} else {
-	        		jsonStr += "\"" + entry.getKey() + "\":\"" + obj + "\"";
-	        	}
-	        }
-
-	        if (ind < (count-1)) {
-	            jsonStr += ",";
-	        }
-	        ind++;
-	    }
-        jsonStr += "}";
-	    return jsonStr;
-	}
 
 }
